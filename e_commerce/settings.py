c9e8d44c397fd98ecc66a54695265533642a9749
Dinesh Basnet <basnetdinesh867@gmail.com>
2025-01-30 20:14:33 +0545

    prod up

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -109,11 +109,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    'static/',
-]
+#STATIC_URL = 'static/'
+#STATICFILES_DIRS = [
+ #   os.path.join(BASE_DIR, 'static'),
+ #   'static/',
+#]
 MEDIA_ROOT = '/media/'
 MEDIA_URL = '/media/'
 
